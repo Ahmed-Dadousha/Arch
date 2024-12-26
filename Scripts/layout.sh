7968b866f0eb8b3ffa4195e753670748cde857af
Ahmed-Dadousha <bioshock44matrix44@gmail.com>
2024-12-26 21:48:53 +0200

    Update: Arabic layout with arabic digits

--- Scripts/layout.sh
+++ Scripts/layout.sh
@@ -5,6 +5,6 @@ current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
 setxkbmap -option caps:escape
 
 [ "$current_layout" = "ara" ] && setxkbmap -layout us
-[ "$current_layout" = "us" ] && setxkbmap -layout ara
+[ "$current_layout" = "us" ] && setxkbmap -layout ara -variant digits
 
 pkill -RTMIN+10 dwmblocks
