133a2b15e662ba52d7d694afa97e881f5a29b453
Ahmed-Dadousha <bioshock44matrix44@gmail.com>
2024-12-26 22:00:04 +0200

    Update: Icon for time

--- Suckless/dwmblocks/blocks.def.h
+++ Suckless/dwmblocks/blocks.def.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
 	{"󰕾 ", "amixer get Master | grep -o '[0-9]*%' | head -n 1", 			1, 	 	0},
 	{" |  ", "sensors | awk '/Core 0/ {print $3}'",             			5, 		0},
 	{" |  ", "setxkbmap -query | grep layout | awk '{print $2}'",			1,		0},
-	{" | 󰥔 ", "date '+%d-%m-%Y (%a) %I:%M:%S:%p   '",				1,		0},
+	{" | 󰸗 ", "date '+%d-%m-%Y (%a) 󰥔 %I:%M:%S:%p   '",				1,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
