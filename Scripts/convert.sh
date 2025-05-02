44d3cc51bd067575927b89ea1ef48d9660d490ef
Ahmed-Dadousha <bioshock44matrix44@gmail.com>
2025-05-03 01:56:45 +0300

    Update: Better code structure

--- Scripts/convert.sh
+++ Scripts/convert.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/bash
 
+# Get input filename without extension
 filename=$(echo "$1" | sed -e 's/\..*$//g')
 
+# If no input
 [ "$#" -eq 0 ] && echo "Please enter a file or a directory to convert." && exit
 
-# Check If the file exists
-if [[ "$2" = "f" ]]; then
-	echo "Converting file $1 to mp3...."
-	[ -f "$1" ] && ffmpeg -n -i "$1" "$filename.mp3" && exit
+# If input is a file
+if [[ -f "$1" ]]; then
+    echo "Converting file $1 to mp3...."
+    ffmpeg -n -i "$1" "$filename.mp3"
 
-	# If file not exists
-	echo "File not exists!"
-
-elif [[ "$2" = "d" ]]; then
-	for input in "$1"/*; do
-		filename=$(echo "$input" | sed -e 's/\..*$//g')
-		echo "Converting file $input to mp3...."
-		[ -f "$input" ] && ffmpeg -n -i "$input" "$filename.mp3"
-	done
+# If input is a directory
+elif [[ -d "$1" ]]; then
+    for input in "$1"/*; do
+        filename=$(echo "$input" | sed -e 's/\..*$//g')
+        echo "Converting file $input to mp3...."
+        [ -f "$input" ] && ffmpeg -n -i "$input" "$filename.mp3"
+    done
+# If input not exists
 else
-	echo "Please enter a letter d or f as argument" && exit
+    echo "Please enter a letter d or f as argument"
 fi
