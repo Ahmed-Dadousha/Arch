b03c03e4a9d33018e4a446561becec72d997cd51
Ahmed-Dadousha <bioshock44matrix44@gmail.com>
2025-05-08 00:01:48 +0300

    Update:

--- Scripts/exit.sh
+++ Scripts/exit.sh
@@ -4,4 +4,4 @@ choice=$(echo -e "⏻ Shutdown\n Reboot\n󰿅 Exit" | dmenu -p " Action: "
 
 [ "$choice" = "⏻ Shutdown" ] && doas poweroff
 [ "$choice" = " Reboot" ] && doas reboot
-[ "$choice" = "󰿅 Exit" ] && pkill dwm
+[ "$choice" = "󰿅 Exit" ] && pkill dwm &>/dev/null
